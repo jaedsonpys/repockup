469c4c4ad41ab0add9a3d1d3403bdad7a0afe3d2
Jaedson Silva <contacanal798@gmail.com>
2022-08-05 22:31:55 -0300

    adding method to get all repositories from user

--- repockup/repockup.py
+++ repockup/repockup.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from typing import Union
 
 import requests
 
@@ -11,3 +12,18 @@ class Repockup(object):
     def __init__(self, username: str, api_token: str = None) -> None:
         self._api_token = api_token
         self._username = username
+
+    def _get_repositories(self) -> Union[list, None]:
+        result = None
+
+        headers = {
+            'Authorization': f'token {self._api_token}'
+        }
+
+        user_url = f'{GITHUB_API}?q=user:{self._username}&per_page={REPO_PER_PAGE}'
+        req = requests.get(user_url, headers=headers)
+
+        if req.status_code == 200:
+            result =  req.json()
+        
+        return result
