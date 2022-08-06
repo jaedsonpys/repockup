183ef5e6dd4a7077ff6c5f0e8766b80f55cb3764
Jaedson Silva <contacanal798@gmail.com>
2022-08-05 22:00:24 -0300

    adding "username" parameter to __init__ method

--- repockup/repockup.py
+++ repockup/repockup.py
@@ -1,9 +1,12 @@
-import shutil
 import os
+import shutil
+
+import requests
 
 GITHUB_API = 'https://api.github.com/users'
 
 
 class Repockup(object):
-    def __init__(self, api_token: str = None) -> None:
+    def __init__(self, username: str, api_token: str = None) -> None:
         self._api_token = api_token
+        self._username = username
