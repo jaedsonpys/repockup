21d44fc8d18f92087bd1f3cc6d94364f4f0b7a77
Jaedson Silva <contacanal798@gmail.com>
2022-08-08 12:56:24 -0300

    adding command line interface to execute backup

--- repockup/__main__.py
+++ repockup/__main__.py
@@ -1,10 +1,12 @@
+import os
+
 import argeasy
 
 from .repockup import Repockup
 from .__init__ import __version__
 
 
-def main():
+def main() -> bool:
     parser = argeasy.ArgEasy(
         project_name='Repockup',
         description='Clone all your repositories with Repockup quickly and simply!',
@@ -17,3 +19,24 @@ def main():
     parser.add_flag('--dest', 'Destination of the repositories ZIP file')
 
     args = parser.get_args()
+
+    if args.backup:
+        username = args.username
+        api_token = args.token
+        file_dest = args.dest
+
+        if not username:
+            print('fatal: username is required')
+            return True
+
+        if file_dest:
+            if not os.path.isdir(file_dest):
+                print(f'fatal: "{file_dest}" is a non-existent directory')
+                return True
+        else:
+            print('fatal: file destination is required')
+
+        repockup = Repockup(username, dest_dir=file_dest, api_token=api_token)        
+        repockup.backup()
+
+    return False
